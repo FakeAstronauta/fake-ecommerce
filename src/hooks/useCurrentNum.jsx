a68ed309a7d6534010464e37d7424c8fde791376
Norman <pescado@crudo.com>
2022-11-23 10:08:30 -0600

    Initial dist subtree commit

--- src/hooks/useCurrentNum.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const useCurrentNum = () => {
-    let [currentNum, setCurrentNum] = React.useState(null);
-    
-    return { currentNum, setCurrentNum }
-}
-
-export default useCurrentNum
--- src/utils/MenuOptions.js
+++
@@ -1,9 +0,0 @@
-export const linksInfo = {
-    whatsnew: "WHAT'S NEW",
-    wear: "READY TO WEAR",
-    accesories: "ACCESSORIES",
-    fragances: "FRAGRANCES",
-    makeup: "MAKEUP",
-    gifts: "GIFTS",
-    stories: "STORIES",
-}
